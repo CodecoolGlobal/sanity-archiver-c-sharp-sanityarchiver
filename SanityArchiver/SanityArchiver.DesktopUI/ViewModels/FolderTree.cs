b462ec28123e296dde7baec117e660255a917def
Flup3r <kotfilemon2101>
2019-11-04 20:57:02 +0100

    fixxing zipping and treeView

--- SanityArchiver/SanityArchiver.DesktopUI/ViewModels/FolderTree.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/FolderTree.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using SanityArchiver.Application.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace SanityArchiver.DesktopUI.ViewModels
 {
     class FolderTree
     {
+        string _FolderPath { get; set; }
+        List<FileProperties> FolderFiles { get; set; }
+
+        public void Populate(string header, string tag, TreeView _root, TreeViewItem _child, bool isfile)
+        {
+            TreeViewItem _driitem = new TreeViewItem();
+            _driitem.Tag = tag;
+            _driitem.Header = header;
+            _driitem.Expanded += new RoutedEventHandler(_driitem_Expanded);
+            if (!isfile)
+            {
+                _driitem.Items.Add(new TreeViewItem());
+            }
+            if (_root != null)
+            {
+                _root.Items.Add(_driitem);
+            }
+            else
+            {
+                _child.Items.Add(_driitem);
+            }
+        }
+
+
+        void _driitem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem _item = (TreeViewItem)sender;
+            List<FileProperties> items = new List<FileProperties>();
+            try
+            {
+                if (_item.Items.Count == 1 && ((TreeViewItem)_item.Items[0]).Header == null)
+                {
+                    _item.Items.Clear();
+                    foreach (string dir in Directory.GetDirectories(_item.Tag.ToString()))
+                    {
+                        DirectoryInfo _dirinfo = new DirectoryInfo(dir);
+                        Populate(_dirinfo.Name, _dirinfo.FullName, null, _item, false);
+                    }
+                    foreach (string dir in Directory.GetFiles(_item.Tag.ToString()))
+                    {
+                        FileInfo _dirinfo = new FileInfo(dir);
+                        items.Add(new FileProperties() { CheckboxName = _dirinfo.FullName, FileName = _dirinfo.Name, CreatedTime = _dirinfo.CreationTime, Size = System.String.Format("{0}KB", _dirinfo.Length / 1024) });
+
+                    }                     
+                    _FolderPath = _item.Tag.ToString();
+                    FolderFiles = items;
+                }
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                MessageBox.Show("Odmowa dostepu");
+            }
+
+        }
+        
+
     }
 }
--- SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,15 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using SanityArchiver.DesktopUI.Converters;
+using SanityArchiver.Application.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Compression;
+using Microsoft.VisualBasic;
+using SanityArchiver.DesktopUI.ViewModels;
+
 
 namespace SanityArchiver.DesktopUI.Views
 {
@@ -14,5 +25,29 @@ namespace SanityArchiver.DesktopUI.Views
         {
             InitializeComponent();
         }
+
+        private void folders_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source is TreeViewItem && ((TreeViewItem)e.Source).IsSelected)
+            {
+                
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var FolderTree = new FolderTree();
+            foreach (DriveInfo driv in DriveInfo.GetDrives())
+            {
+                if (driv.IsReady)
+                {
+                    FolderTree.Folder
+                    FolderTree.Populate(driv.VolumeLabel + "(" + driv.Name + ")", driv.Name, folders, null, false);                    
+                }
+            }
+
+        }
+
     }
 }
